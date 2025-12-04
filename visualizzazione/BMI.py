f3978477fb0b426a9ab8034675dc2f144e9bcee5
lucabalco <luca.balconi04@gmail.com>
2025-12-04 11:39:49 +0100

    Add files via upload

---
+++ visualizzazione/BMI.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ucimlrepo import fetch_ucirepo 
+
+# Caricamento del dataset
+estimation_of_obesity_levels_based_on_eating_habits_and_physical_condition = fetch_ucirepo(id=544) 
+X = estimation_of_obesity_levels_based_on_eating_habits_and_physical_condition.data.features 
+y = estimation_of_obesity_levels_based_on_eating_habits_and_physical_condition.data.targets 
+
+# Unione di features e target
+df = pd.concat([X, y], axis=1)
+df.rename(columns={df.columns[-1]: 'Obesity_Level'}, inplace=True)
+
+# Creazione del BMI
+df['BMI'] = df['Weight'] / (df['Height'] ** 2)
+
+# Visualizzazione BMI senza contorno delle barre e con spazio tra barre
+plt.figure(figsize=(6, 4))
+sns.histplot(
+    df['BMI'],
+    bins=30,
+    color="#12B22D",
+    edgecolor=None,  # rimuove contorno nero
+    shrink=0.9       # lascia uno spazio tra le barre
+)
+
+plt.title('BMI', fontsize=12)
+plt.xlabel('', fontsize=12)
+plt.ylabel('', fontsize=12)
+
+# Personalizzazione assi
+cx = plt.gca() 
+cx.spines['top'].set_visible(False)
+cx.spines['right'].set_visible(False)
+cx.spines['left'].set_visible(False)
+cx.spines['bottom'].set_visible(False)   # rimuove linea asse X
+
+plt.tick_params(bottom=False, left=False)  # rimuove stanghette sui tick
+
+plt.show()
